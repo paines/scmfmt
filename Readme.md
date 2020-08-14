10ebc91d7e92b66f7cdb9f1493df7867c9ca6c52
Anes Lihovac <anes.lihovac@gmail.com>
2020-08-14 11:31:52 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# scmfmt
+
+
+# New Features!
+
+  - A simple sexp pretty printter which work on stdin / stdout
+
+
+You can also:
+  - use vscode extension named "External formatters" to enable this extension if you add 
+ 
+```sh
+ "externalFormatters.languages": {
+        "scheme": {
+            "command": "scmfmt",
+        }
+    }
+```
+
+to settings.json located in ~/.config/Code/User/settings.json or @ext:steefh.external-formatters
---
+++ scmfmt.scm
@@ -0,0 +1,27 @@
+;;simple pretty printer for sexp's
+(import (chicken pretty-print))
+
+(define (copy-line-comment)
+  (let ((char (read-char)))
+    (if (not (eof-object? char))
+      (if (eq? char #\newline)
+        (newline)
+        (begin (write-char char) (copy-line-comment))))))
+
+(define (maintain-empty-lines)
+  (let ((char1 (read-char)) (char2 (peek-char)))
+    (if (and (eq? char1 #\newline) (eq? char2 #\newline))
+      (write-char (read-char)))))
+
+;; The main method. This reads from and writes to stdin/stdout.
+(define (scmfmt)
+  (let ((char (peek-char)))
+    (if (not (eof-object? char))
+      (begin
+        (cond ((eq? char #\;) (copy-line-comment))
+              ((eq? char #\newline) (maintain-empty-lines))
+              ((char-whitespace? char) (read-char))
+              (#t (pretty-print (read))))
+        (scmfmt)))))
+
+(scmfmt)
